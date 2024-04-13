df362cc316956cb61ba57b334c9f81879cfdd051
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-04-13 19:01:44 +0530

    Create multi-attention.py

---
+++ utils/multi-attention.py
@@ -0,0 +1,42 @@
+class MultiHeadSelfAttention(tf.keras.layers.Layer):
+  def __init__(self, d_model, num_heads):
+    super(MultiHeadSelfAttention, self).__init__()
+    #class is taking model dimension and no of heads as arguments
+    self.d_model = d_model
+    self.num_heads = num_heads
+
+    self.d_head = self.d_model // self.num_heads
+
+    # we make 3 weight layers for q, k and value. 
+    self.wq = tf.keras.layers.Dense(self.d_model)
+    self.wk = tf.keras.layers.Dense(self.d_model)
+    self.wv = tf.keras.layers.Dense(self.d_model)
+
+    # Linear layer to generate the final output.
+    self.dense = tf.keras.layers.Dense(self.d_model)
+  
+  def split_heads(self, x):
+    batch_size = x.shape[0]
+
+    split_inputs = tf.reshape(x, (batch_size, -1, self.num_heads, self.d_head))
+    return tf.transpose(split_inputs, perm=[0, 2, 1, 3])
+  
+  def merge_heads(self, x):
+    batch_size = x.shape[0]
+
+    merged_inputs = tf.transpose(x, perm=[0, 2, 1, 3])
+    return tf.reshape(merged_inputs, (batch_size, -1, self.d_model))
+
+  def call(self, q, k, v, mask):
+    qs = self.wq(q)
+    ks = self.wk(k)
+    vs = self.wv(v)
+
+    qs = self.split_heads(qs)
+    ks = self.split_heads(ks)
+    vs = self.split_heads(vs)
+
+    output, attn_weights = scaled_dot_product_attention(qs, ks, vs, mask)
+    output = self.merge_heads(output)
+
+    return self.dense(output), attn_weights
