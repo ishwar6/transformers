aff41149fd6a69acb6293404d23d77c550bbfb69
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-04-16 16:21:19 +0530

    Update bitsandbytes.py

--- fine-tuning/bitsandbytes.py
+++ fine-tuning/bitsandbytes.py
@@ -60,5 +60,50 @@ bnb_config = BitsAndBytesConfig(
         bnb_4bit_use_double_quant=False,
     )
 
+# load_in_8bit (bool, optional, defaults to False) — This flag is used to enable 8-bit quantization with LLM.int8().
+# load_in_4bit (bool, optional, defaults to False) — This flag is used to enable 4-bit quantization by replacing the Linear layers with FP4/NF4 layers from bitsandbytes.
+
+
+# AutoModelForCausalLM.from_pretrained(): This function loads a pre-trained model given a model name. It automatically downloads the model's weights and configures it for use.
+
+model_name='microsoft/phi-2'
+device_map = {"": 0}
+# device_map: This dictionary is used to control the mapping of the model's layers to specific devices (like different GPUs). 
+# The key-value pair "": 0 indicates that all layers of the model should be loaded onto the device with ID 0 (typically the first GPU). 
+
+original_model = AutoModelForCausalLM.from_pretrained(model_name, 
+                                                      device_map=device_map,
+                                                      quantization_config=bnb_config,
+                                                      trust_remote_code=True,
+                                                      use_auth_token=False)
+
+
+# Here not the quantization config: . It tells the model loading function to apply certain quantization parameters right from the start, which affects how the model weights are loaded and potentially how they are stored in memory.
+# This is same we created above. This is part of optimizing the model's size and the speed of its operations.
+
+
+
+
+# ####### Tokenizer #######
+# This method is used to instantiate a tokenizer associated with a specific pre-trained model. 
+# It automatically selects the correct tokenizer class based on the model identifier and loads its pre-trained configuration and vocabulary.
+    
+tokenizer = AutoTokenizer.from_pretrained(model_name,trust_remote_code=True,padding_side="left",add_eos_token=True,add_bos_token=True,use_fast=False)
+tokenizer.pad_token = tokenizer.eos_token
+
+
+
+# # what is pad token?
+    # This line sets the padding token (pad_token) of the tokenizer to be the same as the end-of-sequence token (eos_token). 
+    # In many transformer models, especially autoregressive models like GPT, the eos_token is used to indicate the end of a sequence. By setting the pad_token to eos_token, 
+    # we're ensuring that any padding added to sequences during batch preparation is treated as part of the sequence's normal flow, which can be crucial for maintaining consistency in models' expectations about input structure.
+
+
+
+
+
+
+
+
 
 
