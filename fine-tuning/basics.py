56d2410d017f4202e41ad1aa1406cf0ad3f00eec
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-04-16 11:18:33 +0530

    Update basics.py

--- fine-tuning/basics.py
+++ fine-tuning/basics.py
@@ -1,1 +1,4 @@
 # why? : to enhance their performance (LLMs) on specific tasks by adapting them to domain-specific data. 
+
+# Fine-tuning LLM involves the additional training of a pre-existing model, which has previously acquired patterns and features from an extensive dataset, using a smaller, domain-specific dataset.
+# Utilizing the existing knowledge embedded in the pre-trained model allows for achieving high performance on specific tasks with substantially reduced data and computational requirements.
