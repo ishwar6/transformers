0ff8631abafbb75b9dd3c42d96c462a86381dd90
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-04-16 11:22:59 +0530

    Update basics.py

--- fine-tuning/basics.py
+++ fine-tuning/basics.py
@@ -17,3 +17,13 @@ The dataset which we will select might be related to a particular domain or appl
 
 # Task-specific adaptation: During fine-tuning, the model’s parameters are adjusted based on the new dataset, helping it better understand and generate content relevant to the specific task. 
 This process retains the general language knowledge gained during pre-training while tailoring the model to the nuances of the target domain.
+
+
+# #Fine-tuning methods: 
+#   1. Full Fine Tuning (Instruction fine-tuning) : updates all model weights, creating a new version with improved capabilities. However, it demands sufficient memory and computational resources, similar to pre-training,
+#   2. Parameter Efficient Fine-Tuning (PEFT) is a form of instruction fine-tuning that is much more efficient than full fine-tuning. PEFT addresses this by updating only a subset of parameters, effectively “freezing” the rest. 
+      # This reduces the number of trainable parameters, making memory requirements more manageable and preventing catastrophic forgetting.
+# There are various ways of achieving Parameter efficient fine-tuning. Low-Rank Adaptation LoRA & QLoRA are the most widely used and effective.
+
+# LoRA: Low-Rank Adaptation of Large Language Models
+# Edward J. Hu, Yelong Shen, Phillip Wallis, Zeyuan Allen-Zhu, Yuanzhi Li, Shean Wang, Lu Wang, Weizhu Chen
