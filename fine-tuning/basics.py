fd3ebbd53dee923eddf67389fadf9ed379c3f70f
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-04-16 11:19:50 +0530

    Update basics.py

--- fine-tuning/basics.py
+++ fine-tuning/basics.py
@@ -2,3 +2,18 @@
 
 # Fine-tuning LLM involves the additional training of a pre-existing model, which has previously acquired patterns and features from an extensive dataset, using a smaller, domain-specific dataset.
 # Utilizing the existing knowledge embedded in the pre-trained model allows for achieving high performance on specific tasks with substantially reduced data and computational requirements.
+
+
+# key steps involved in LLM Fine-tuning:
+
+# Select a pre-trained model: For LLM Fine-tuning first step is to carefully select a base pre-trained model that aligns with our desired architecture and functionalities. 
+
+# Gather relevant Dataset: Then we need to gather a dataset that is relevant to our task. The dataset should be labeled or structured in a way that the model can learn from it.
+
+# Preprocess Dataset: Once the dataset is ready, we need to do some preprocessing for fine-tuning by cleaning it, splitting it into training, validation, and test sets, and ensuring it’s compatible with the model on which we want to fine-tune.
+
+# Fine-tuning: After selecting a pre-trained model we need to fine tune it on our preprocessed relevant dataset which is more specific to the task at hand. 
+The dataset which we will select might be related to a particular domain or application, allowing the model to adapt and specialize for that context.
+
+# Task-specific adaptation: During fine-tuning, the model’s parameters are adjusted based on the new dataset, helping it better understand and generate content relevant to the specific task. 
+This process retains the general language knowledge gained during pre-training while tailoring the model to the nuances of the target domain.
