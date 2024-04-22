b00bbb8da0679a3c5b2e77114f129ca34ed1954f
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-04-22 22:08:01 +0530

    Update bitsandbytes.py

--- fine-tuning/bitsandbytes.py
+++ fine-tuning/bitsandbytes.py
@@ -6,7 +6,6 @@
 # QLoRA or 4-bit quantization enables large language model training with several memory-saving techniques that don’t compromise performance. 
     #This method quantizes a model to 4-bits and inserts a small set of trainable low-rank adaptation (LoRA) weights to allow training.
 
-
 # 8-bit optimizers
 # With 8-bit optimizers, large models can be finetuned with 75% less GPU memory without losing any accuracy compared to training with standard 32-bit optimizers. 
 # The reduced memory requirements means 8-bit optimizers are 4x faster than a standard optimizer, and no hyperparameter tuning is required.
