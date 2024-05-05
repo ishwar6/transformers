797cf0383f5dee128db9683a632ae9f8102f53e7
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-05-06 02:02:29 +0530

    GLUE

---
+++ autoencoder/evaluate.py
@@ -0,0 +1,32 @@
+# The General Language Understanding Evaluation (GLUE) benchmark is a collection of diverse natural language understanding (NLU) tasks. 
+# It is designed to evaluate the performance of machine learning models across a broad range of linguistic challenges. Developed by researchers at New York University and 
+# the University of Washington, GLUE serves as a standard benchmark for testing and comparing NLP models.
+
+
+# Key Aspects of GLUE:
+# Tasks:
+# GLUE consists of nine different tasks, each testing different aspects of language understanding. These tasks include:
+# CoLA (Corpus of Linguistic Acceptability): Binary classification of whether a sentence is grammatically correct or not.
+# SST-2 (Stanford Sentiment Treebank): Sentiment classification into positive or negative classes.
+# MRPC (Microsoft Research Paraphrase Corpus): Binary classification of whether two sentences are paraphrases of each other.
+# STS-B (Semantic Textual Similarity Benchmark): Regression task to predict the similarity score between two sentences.
+# QQP (Quora Question Pairs): Determine whether two given questions are duplicates or not.
+# MNLI (Multi-Genre Natural Language Inference): Multi-class classification to predict the relationship between a premise and hypothesis.
+# QNLI (Question Natural Language Inference): Determine if a given sentence answers a corresponding question.
+# RTE (Recognizing Textual Entailment): Binary classification to check textual entailment between sentences.
+# WNLI (Winograd NLI): Binary classification to resolve ambiguous pronouns in sentences.
+
+
+# Evaluation:
+# Each task has specific metrics for evaluating model performance, such as accuracy, F1 scores, and Pearson correlation.
+  
+# Training and Testing:
+# The GLUE benchmark provides training, validation, and test sets for each task. The test set is used to evaluate the generalization capabilities of NLP models.
+
+# Purpose:
+# GLUE allows researchers to evaluate the robustness and general language understanding of machine learning models across a variety of tasks.
+# By testing multiple tasks, it ensures that models don't overfit to a specific task but can generalize well to other NLP challenges.
+
+
+# Successors:
+# SuperGLUE is an advanced version of GLUE that offers more challenging tasks for evaluating state-of-the-art models.
