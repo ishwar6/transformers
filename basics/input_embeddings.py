63830e1ce756c7036be5f04a5e57ec1e8ef6e667
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2025-02-11 00:35:24 +0530

    comments added

--- basics/input_embeddings.py
+++ basics/input_embeddings.py
@@ -14,3 +14,18 @@ class InputEmbedding(nn.Module):
     return self.embedding(x) * math.sqrt(self.d_model)
 
 
+
+###### Explanation ####
+
+# d_model: The dimension of the embedding vector (also the model’s hidden size).
+# vocab_size: The size of the vocabulary (number of tokens).
+# Embedding Layer: nn.Embedding(vocab_size, d_model) creates a lookup table that maps each token (represented by an integer index) to a dense vector of size d_model.
+
+# Forward Pass:
+# Input: x is typically a tensor of shape (batch_size, seq_length) containing token indices.
+# Embedding Lookup: self.embedding(x) retrieves the dense representation for each token.
+# scaling is there: 
+# In the original Transformer paper "Attention Is All You Need", 
+# scaling is performed to counteract the effect of the dot-product growing large in magnitude when the dimensionality is high. 
+#   This helps in stabilizing gradients early in training.
+                                        
