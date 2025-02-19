839a61e2b8ba84e68b6d407bac20aa8ffd11610e
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2025-02-19 17:53:17 +0530

    Update 1. block.py

--- attention/1. block.py
+++ attention/1. block.py
@@ -7,7 +7,21 @@
 # lets talk about input (seq_length, d_model): we will have Q, K, V each of same (seq_length, d_model)
 # we multiply Q by W_q, K by W_k and V by W_v (each weights are of d_model * d_model) : we after multiply get result of (seq_length, d_model)
 # we divide this new Q, new K and new V into h blocks (h of multihead attentions): we split matrix along embedding dimension: 
-# which means each head get full sentence (sequence) but diff part of embedding of each word. 
+# which means each head get full sentence (sequence) but diff part of embedding of each word. : #attending to different parts of the input representation
 # we apply attention formula to each head and finally combine by concat formula. 
+
+
+import torch
+import torch.nn as nn
+
+class MultiHeadAttentionBlock(nn.module):
+  def __init__(self, d_model: int, h: int, dropout: int) -> None:
+    super().__init__()
+    self.d_model = d_model
+    self.h = h
+    assert d_model % h == 0 "No of heads should be divisible by d_model"
+    
+
+
 
 
